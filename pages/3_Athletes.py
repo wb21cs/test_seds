de6eac23bc6b6067de49e00f0537ecc24c428d04
BEN TERKI Abdel Ouakil <mustaben94@gmail.com>
2025-12-06 16:39:39 +0100

    make athlete profile

--- pages/3_Athletes.py
+++ pages/3_Athletes.py
@@ -1,9 +1,10 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import ast
+
 
 athletes = pd.read_csv("data/athletes.csv")
-st.write(str(np.array(athletes.loc[athletes["code"] == 1532872, "name"])[0]))
 
 st.title("Athletes")
 selected_athlete = st.selectbox(label="Select an athlete:",
@@ -12,54 +13,60 @@ selected_athlete = st.selectbox(label="Select an athlete:",
              placeholder="Choose an athlete...",
              format_func=lambda opt: str(np.array(athletes.loc[athletes["code"] == opt, "name"])[0])
              )
-selected_athlete
 
 
-st.subheader("Selected Athlete Profile")
+if selected_athlete:
+    athlete = (athletes.loc[
+            athletes["code"] == selected_athlete,
+            ["name", "country", "height", "weight", "disciplines", "events", "coach"]
+        ]
+        .iloc[0]
+        .to_dict()
+    )
 
-# --- PLACEHOLDER DATA (replace later with real values) ---
-athlete = {
-    "name": "Select an athlete",
-    "country": "N/A",
-    "height": "—",
-    "weight": "—",
-    "sports": "—",
-    "disciplines": "—",
-    "coaches": "—",
-    "image": None  # or a placeholder image URL later
-}
 
-# --- PROFILE CARD LAYOUT ---
-col1, col2 = st.columns([1, 3], gap="large")
+    
+    def get_athlete_image(name):
+        return "https://img.olympics.com/images/image/private/t_1-1_300/f_auto/primary/pgotjrtojoadz7ylp7gv"
 
-with col1:
-    st.image("https://img.olympics.com/images/image/private/t_1-1_300/f_auto/primary/pgotjrtojoadz7ylp7gv", width=160)
-    # if athlete["image"]:
-    #     st.image(athlete["image"], width=160)
-    # else:
-    #     st.markdown(
-    #         """
-    #         <div style="
-    #             width:160px;
-    #             height:160px;
-    #             border-radius:50%;
-    #             background:#e5e7eb;
-    #             display:flex;
-    #             align-items:center;
-    #             justify-content:center;
-    #             font-size:48px;
-    #             color:#6b7280;">
-    #             👤
-    #         </div>
-    #         """,
-    #         unsafe_allow_html=True
-    #     )
+    athlete['disciplines'] = ast.literal_eval(athlete['disciplines'])
+    athlete['events'] = ast.literal_eval(athlete['events'])
+    athlete['image'] = get_athlete_image(athlete['name'])
 
-with col2:
-    st.markdown(f"### {athlete['name']}")
-    st.markdown(f"**Country / NOC:** {athlete['country']}")
-    st.markdown(f"**Height:** {athlete['height']} cm")
-    st.markdown(f"**Weight:** {athlete['weight']} kg")
-    st.markdown(f"**Sport(s):** {athlete['sports']}")
-    st.markdown(f"**Discipline(s):** {athlete['disciplines']}")
-    st.markdown(f"**Coach(s):** {athlete['coaches']}")
+    
+    st.subheader("Selected Athlete Profile")
+
+    col1, col2 = st.columns([1, 3], gap="large")
+
+    with col1:
+        if athlete["image"]:
+            st.image(athlete["image"], width=160)
+        else:
+            st.markdown(
+                """
+                <div style="
+                    width:160px;
+                    height:160px;
+                    border-radius:50%;
+                    background:#e5e7eb;
+                    display:flex;
+                    align-items:center;
+                    justify-content:center;
+                    font-size:48px;
+                    color:#6b7280;">
+                    👤
+                </div>
+                """,
+                unsafe_allow_html=True
+            )
+
+    with col2:
+        st.markdown(f"### {athlete['name']}")
+        st.markdown(f"**Country:** {athlete['country']}")
+        st.markdown(f"**Height:** {athlete['height']} cm")
+        st.markdown(f"**Weight:** {athlete['weight']} kg")
+        st.markdown(f"**Sport(s):** {", ".join(athlete['disciplines'])}")
+        st.markdown(f"**Events(s):** {", ".join(athlete['events'])}")
+        st.markdown(f"**Coach(s):** {athlete['coach'].replace(".<br>", ", ")}")
+
+
